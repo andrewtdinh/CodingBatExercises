b51451a8c97a2a3f2c699bc13aa2bda20f7381a5
Andrew Dinh <andrewdinh74@yahoo.com>
2015-11-21 00:07:53 -0800

    Fix count_evens file

--- list_1/count_evens.py
+++ list_1/count_evens.py
@@ -1,6 +1,2 @@
 def count_evens(nums):
-  evens = [1 for num in nums if num % 2 == 0]
-  print evens
-  return reduce(lambda x, y: x + y, evens)
-
-count_evens([3,5,2,4, 6])
+  return len([1 for num in nums if num % 2 == 0])
