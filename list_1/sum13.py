24234dc95921f4dfdc6605a21030cab98a6a8a17
Andrew Dinh <andrewdinh74@yahoo.com>
2015-11-23 00:24:30 -0800

    Make sum13 file works

--- list_1/sum13.py
+++ list_1/sum13.py
@@ -1,4 +1,10 @@
 def sum13(nums):
   if nums:
+    indicesOf13 = [i for i in range(len(nums)) if nums[i] == 13]
+    blackList = indicesOf13 + [indx + 1 for indx in indicesOf13]
+    if indicesOf13:
+      return sum([nums[i] for i in range(len(nums)) if i not in blackList])
+    else:
+      return sum(nums)
   else:
-    return 0 
+    return 0
