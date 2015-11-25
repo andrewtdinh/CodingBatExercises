d78431a48666c90d174e76649bafbc45573670b1
Andrew Dinh <andrewdinh74@yahoo.com>
2015-11-25 10:06:32 -0800

    Modify sum67 file again

--- list_1/sum67.py
+++ list_1/sum67.py
@@ -3,5 +3,5 @@ def sum67(nums):
   indices_6 = [i for i in range(arr_length) if nums[i]==6]
   indices_7 = [i for i in range(arr_length) if nums[i]==7]
   for index, startPos in enumerate(indices_6):
-    nums = nums[:startPos] + nums[indices_7[index] + 1:]
+    del nums[startPos:indices_7[index] + 1]
   return nums
