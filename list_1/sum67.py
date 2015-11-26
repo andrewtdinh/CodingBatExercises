74bcb307bfd41c179562628f7be288e7cdcbb362
Andrew Dinh <andrewdinh74@yahoo.com>
2015-11-25 19:20:10 -0800

    Infinite loop

--- list_1/sum67.py
+++ list_1/sum67.py
@@ -1,4 +1,5 @@
 def sum67(nums):
-  while ((6 in nums and 7 in nums) and (nums.index(6) < nums.index(7))):
-    del nums[nums.index(6):nums.index(7) + 1]
+  while (6 in nums and 7 in nums[nums.index(6):]):
+    index7 = nums[nums.index(6):].index(7)
+    del nums[nums.index(6):index7 + 1]
   return sum(nums)
