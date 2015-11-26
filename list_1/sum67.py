baa65b4c1f60655fc3c0136b90cfe7ea83a08dbe
Andrew Dinh <andrewdinh74@yahoo.com>
2015-11-25 20:46:32 -0800

    Fix sum67

--- list_1/sum67.py
+++ list_1/sum67.py
@@ -1,5 +1,6 @@
 def sum67(nums):
-  while (6 in nums and 7 in nums[nums.index(6):]):
-    index7 = nums[nums.index(6):].index(7)
-    del nums[nums.index(6):index7 + 1]
+  while 6 in nums and 7 in nums[nums.index(6):]:
+    index6 = nums.index(6)
+    index7 = nums[index6:].index(7) + index6
+    nums = nums[:index6] + nums[(index7 + 1):]
   return sum(nums)
