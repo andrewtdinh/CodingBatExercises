5db84f300c02a4289b4d54c4013790a37a249071
Andrew Dinh <andrewdinh74@yahoo.com>
2015-11-25 17:41:37 -0800

    more troubleshooting sum67

--- list_1/sum67.py
+++ list_1/sum67.py
@@ -1,9 +1,8 @@
 def sum67(nums):
-  arr_length = len(nums)
-  indices_7 = [i for i in range(len(nums)) if nums[i]==7]
-  indices_6 = [i for i in range(len(indices_7)) if nums[i]==6]
-  for index, startPos in enumerate(indices_6):
-    del nums[startPos:indices_7[index] + 1]
+  next6 = nums.find(6)
+  while (next6 and nums.find(7, next6)):
+    del nums[next6:nums.find(7, next6)]
+    next6 = nums.find(6)
   return nums
-  
+
 arr = [1, 3, 5, 6, 3, 5, 6, 7]
